1bd9d347d9df4c011c685a5d0ff483a78be780b0
Chris Struck <chris.struck@cstruck.dev>
2023-05-05 03:54:52 -0500

    Started layout for commands, need to test as last time node-ssq had strange issues.

--- bot.js
+++ bot.js
@@ -6,9 +6,9 @@
 
 // Variables
 
-const ssq = require('node-ssq');					// Interfaces with Source servers
-const Discord = require('discord.js');				// Discord bot API
-const client = new Discord.Client();				// Discord bot client
+const ssq = require('ssq');					// Interfaces with Source servers
+// const Discord = require('discord.js');				// Discord bot API
+// const client = new Discord.Client();				// Discord bot client
 
 const serverinfo = require('./server.json');		// Info relating to the target server
 const config = require('./config.json');			// Info relating to the bot configuration
@@ -16,4 +16,37 @@ const config = require('./config.json');			// Info relating to the bot configura
 // Initialization
 
 // Set ssq timeout to 2000ms (DEFAULT)
-ssq.setTimeout(2000);
+// ssq.set_timeout(2000);
+
+
+// Main
+switch('players')
+{
+	case 'test':
+	case 'ping':
+		ssq.ping(serverinfo.serverip, serverinfo.serverport, (err, data) => {
+			console.log('Pinging server ...\n' + data)
+			console.error(err)
+		});
+		break;
+	case 'server':
+	case 'status':
+		ssq.info(serverinfo.serverip, serverinfo.serverport, (err, data) => {
+			console.log('Pinging server ...\n' + data)
+			console.error(err)
+		});
+		break;
+	case 'players':
+	case 'playerlist':
+		ssq.players(serverinfo.serverip, serverinfo.serverport, (err, data) => {
+			console.log('Pinging server ...\n' + data)
+			console.error(err)
+		});
+		break;
+	case 'help':
+	case 'info':
+	case 'cmds':
+		break;
+	default:
+		break;
+}
